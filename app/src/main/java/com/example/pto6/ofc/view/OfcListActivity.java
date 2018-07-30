bff8ef419359cb36c4e2f6751d5cd87e7ef68889
nimtego <nimtego@mail.ru>
2018-07-30 22:56:46 +0300

    #21 Реализация макета (есть косяк с тайлом пока wip)

--- app/src/main/java/com/example/pto6/ofc/view/OfcListActivity.java
+++ app/src/main/java/com/example/pto6/ofc/view/OfcListActivity.java
@@ -3,6 +3,9 @@ package com.example.pto6.ofc.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -11,9 +14,47 @@ import com.example.pto6.ofc.presenter.OfcListPresenter;
 import com.example.pto6.ofc.presenter.Presenter;
 
 
-public class OfcListActivity extends AbstractView {
 
+public class OfcListActivity extends AbstractView {
     private RecyclerView mRecyclerView;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ofc_list);
+        this.mRecyclerView = findViewById(R.id.recycler_view);
+        setUpRecyclerView();
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Credit"));
+        tabs.addTab(tabs.newTab().setText("Debit"));
+        tabs.addTab(tabs.newTab().setText("Data"));
+        this.fab = findViewById(R.id.fab);
+        fab.setOnClickListener(mPresenter);
+        mPresenter.viewReady();
+    }
+
+    private void setUpRecyclerView() {
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    @Override
+    public Presenter setPresenter() {
+        return new OfcListPresenter();
+    }
+
+    public void setUserFinance(RecyclerView.Adapter mAdapter) {
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    /*private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     private FloatingActionButton fab;
 
@@ -36,5 +77,5 @@ public class OfcListActivity extends AbstractView {
 
     public void setUserFinance(RecyclerView.Adapter mAdapter) {
         mRecyclerView.setAdapter(mAdapter);
-    }
+    }*/
 }
